cd386bb3c1aedaa8d73a1c50ef6633956616305c
Apurv Singh <apurvsingh934@gmail.com>
2026-03-22 11:25:28 +0530

    server file changed

--- server/server.js
+++ server/server.js
@@ -38,7 +38,6 @@ app.use("/api/product", productRouter)
 app.use("/api/cart", cartRouter)
 app.use("/api/address", addressRouter)
 app.use("/api/order", orderRouter)
-app.use()
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
